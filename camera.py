83e2f86a57617c88f4ff86829bb77c6e5ebf6f03
Luke Reinke <LukeReinke@electrogatorrobotics.com>
2019-09-28 16:15:59 -0500

    changed kill state

--- camera.py
+++ camera.py
@@ -13,7 +13,7 @@ gpio.setup(kill, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 camera.resolution = (150, 100)
 camera.start_preview()
-while gpio.input(kill)==True:
+while gpio.input(kill)==False:
     if gpio.input(triger)==True:
         print("True, %s" % img_num)
         camera.capture('snapshot%s.jpeg' % img_num, resize=(720,480))
