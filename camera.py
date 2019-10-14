edb5148e9dd060ed37a040efd9915743b328a4d6
Luke Reinke <LukeReinke@electrogatorrobotics.com>
2019-10-14 17:20:29 -0500

    Changed image Res and reduced delay between images

--- camera.py
+++ camera.py
@@ -9,14 +9,13 @@ camera = picamera.PiCamera()
 gpio.setmode(gpio.BOARD)
 gpio.setup(triger,  gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
-camera.resolution = (150, 100)
 camera.start_preview()
 while True:
 
     if gpio.input(triger)==True:
-        print("True, %s" % img_num)
-        camera.capture('snapshot%s.jpeg' % img_num, resize=(720,480))
+        print("%s" % img_num)
+        camera.capture('snapshot%s.jpeg' % img_num, resize=(1080,1920))
         img_num+=1
-        sleep(0.5)
+        sleep(0.2)
 camera.stop_preview()
     
